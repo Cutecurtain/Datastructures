ba2cb2bb0f9414c701b2ed2f2a52f8019cd1f668
cutecurtain <aron.sandstedt@live.se>
2018-03-02 16:42:11 +0100

    add more comments?

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -212,7 +212,7 @@ public class DirectedGraph<E extends Edge> {
 	private static <E extends Edge> void rePoint(List<E>[] cc, int to, int from) {
 		// Append elements from the list in 'from' to the list in 'to'.
 		cc[to].addAll(cc[from]);
-		
+
 		// Make every affected node reference the to list.
 		for (E edge : cc[from]) {
 			cc[edge.getSource()] = cc[to];
